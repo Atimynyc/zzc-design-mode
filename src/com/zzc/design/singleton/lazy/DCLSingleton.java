43f5e7dd779fcef1681553249084a7711fc18953
zhengzhenchao <zhenchao.zheng@swiftpass.cn>
2021-05-18 17:58:22 +0800

    [新增功能](main): 单例模式 init

---
+++ src/com/zzc/design/singleton/lazy/DCLSingleton.java
@@ -0,0 +1,24 @@
+package com.zzc.design.singleton.lazy;
+
+/**
+ * 双重校验锁（DCL，即double-checked locking）
+ * 双重校验锁相较于懒汉式 + 线程安全来说，提升了性能，它不会每次都将方法锁住，只会第一次初始化的时候锁住这个单例。
+ */
+public class DCLSingleton {
+    // 这里需要有个volatile关键字，防止初始化对象的时候重排序了，导致开拓对象空间提前于生成对象，
+    // 这样会导致一个线程在初始化对象。另一个线程发现这个对象的空间都已经有了，去拿后发现这个空间里面啥东西都没有，会导致拿到一个null
+    private volatile static DCLSingleton dclSingleton;
+    private DCLSingleton(){}
+
+    public static DCLSingleton getDclSingleton() {
+        if (dclSingleton == null) {
+            synchronized (DCLSingleton.class) {
+                if (dclSingleton == null) {
+                    dclSingleton = new DCLSingleton();
+                }
+            }
+        }
+        return dclSingleton;
+    }
+
+}
---
+++ src/com/zzc/design/singleton/lazy/LazySingletonWithoutThreadSafe.java
@@ -0,0 +1,22 @@
+package com.zzc.design.singleton.lazy;
+
+/**
+ * 懒汉式，线程不安全
+ * 懒汉的意思是，等到要用的时候，才初始化该单例。（翻译过来就是不到deadline你是不会完成论文的）
+ * 由于懒汉式是要在用的时候再初始化，你没法控制这个时候是不是两个人同时过来初始化，所以我们要考虑线程安全的问题。
+ * 当两个线程同时进来，发现instance == null， 那么就会new 两次该单例
+ */
+public class LazySingletonWithoutThreadSafe {
+
+    private static LazySingletonWithoutThreadSafe instance;
+
+    private LazySingletonWithoutThreadSafe(){}
+
+    public static LazySingletonWithoutThreadSafe getInstance() {
+        if (instance == null) {
+            instance = new LazySingletonWithoutThreadSafe();
+        }
+        return instance;
+    }
+
+}
