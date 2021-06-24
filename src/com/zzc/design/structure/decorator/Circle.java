d951f81784aa2269d6419de8bb6aab8ed8f0b504
Atimynyc <495906027@qq.com>
2021-06-24 19:32:12 +0800

    [新增功能](main): 装饰者模式 1.添加装饰者模式例子 2.添加装饰者模式概述

---
+++ src/com/zzc/design/structure/decorator/Circle.java
@@ -0,0 +1,8 @@
+package com.zzc.design.structure.decorator;
+
+public class Circle implements Shape {
+    @Override
+    public void draw() {
+        System.out.println("Shape: Circle");
+    }
+}
---
+++ src/com/zzc/design/structure/decorator/Shape.java
@@ -0,0 +1,5 @@
+package com.zzc.design.structure.decorator;
+
+public interface Shape {
+    void draw();
+}
